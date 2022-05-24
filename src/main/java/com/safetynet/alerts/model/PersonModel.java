8ec53e07d6f8502d1de9241cecc4ff7178a53897
Jérôme Pagny <pagny.jerome@gmail.com>
2022-05-24 22:04:03 +0200

    Revert "Change method to set ID Person"
    This reverts commit d63d238a25214545ded3dba894d7f2de1480e1f6.

--- src/main/java/com/safetynet/alerts/model/PersonModel.java
+++ src/main/java/com/safetynet/alerts/model/PersonModel.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Locale;
-
 @Getter
 @Setter
 @AllArgsConstructor
@@ -28,7 +26,7 @@ public class PersonModel {
                        final String theAddress, final String theCity,
                        final String theZip, final String thePhone,
                        final String theEmail) {
-        this.id = theFirstName.toLowerCase() + theLastName.toLowerCase();
+        this.id = theFirstName.substring(0,3) + theLastName.substring(0,3);
         this.firstName = theFirstName;
         this.lastName = theLastName;
         this.address = theAddress;
