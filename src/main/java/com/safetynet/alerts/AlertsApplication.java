c227c3052c7c9eb5725bb13f58c396dcf59d2bf0
Jérôme Pagny <pagny.jerome@gmail.com>
2022-05-24 17:39:20 +0200

    Add logs

--- src/main/java/com/safetynet/alerts/AlertsApplication.java
+++ src/main/java/com/safetynet/alerts/AlertsApplication.java
@@ -3,8 +3,6 @@ package com.safetynet.alerts;
 import com.safetynet.alerts.config.LoadDataFromDataJson;
 import com.safetynet.alerts.model.DataFromJsonFile;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
